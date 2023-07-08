578beb7044a1ab7634eb8ce82e77fe4ce8f44f4f
FLY123 <50164284+akfakf0509@users.noreply.github.com>
2023-07-08 19:53:00 +0900

    feat: 전체적인 페이지 작성

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
-import { useMatch } from "~/stores/match";
+import { useMatch } from "@/stores/match";
 
 const { value } = useMatch();
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li v-for="(i, index) in value.team" :key="`team-${index}`">i</li>
+  <div class="container">
+    <h1 class="title">Match</h1>
+    <ul class="member-list">
+      <li v-for="i in 10" :key="`team-${i}`" class="member-list__item">
+        <Member v-bind="value.members[i]" />
+      </li>
     </ul>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+  padding: 40px 0;
+  margin: 0 400px;
+}
+.title {
+  @include font-heading-xl;
+
+  text-align: center;
+  margin-bottom: 32px;
+}
+
+.member-list {
+  flex: 1;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  align-items: stretch;
+  gap: 16px;
+}
+</style>
